4e34f72da2e0860a92ee4a0a7132d9411a097f26
Amanuel <Amanuel.Eshete1@gmail.com>
2024-03-30 10:03:30 +0300

    Added RateLimiter using remote Ip address

--- search_suggestion.api/search_suggestion.api/Program.cs
+++ search_suggestion.api/search_suggestion.api/Program.cs
@@ -1,6 +1,7 @@
 using search_suggestion.application.Contracts.Search;
 using search_suggestion.application.Service.Search;
 using System.Reflection;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,7 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+AddRateLimiter(builder.Services);
 // Add HttpClient as a singleton
 builder.Services.AddSingleton<HttpClient>();
 
@@ -33,8 +34,23 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRateLimiter();
 app.UseAuthorization();
 app.MapControllers();
 app.UseCors("AllowAll");
 
 app.Run();
+void AddRateLimiter(IServiceCollection services)
+{
+    services.AddRateLimiter(option =>
+    {
+        option.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+        option.AddPolicy("FixedPolicy", httpContent => RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContent.Connection.RemoteIpAddress?.ToString() ,
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 10,
+                Window = TimeSpan.FromSeconds(20)
+            }));
+    });
+}
